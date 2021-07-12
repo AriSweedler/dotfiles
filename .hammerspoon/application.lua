4fd7b8a80eda620594f9da354858899ee7d7f375
Ari Sweedler <ari@sweedler.com>
2021-07-11 23:55:14 -0600

    Standardized hammerspoon volume-and-brightness

--- .hammerspoon/application.lua
+++ .hammerspoon/application.lua
@@ -1,15 +1,17 @@
 -- hs.application.frontmostApplication()
 
 local key_app = {
-T = "Terminal",
-F = "Firefox",
 C = "Calendar",
--- M = "Messages",
-S = "Signal",
+F = "Firefox",
+I = "Signal",
+M = "Messages",
+S = "Spotify",
+T = "Terminal",
 }
 
+application_select = {"cmd", "alt"}
 for key, app in pairs(key_app) do
-  hs.hotkey.bind({"cmd", "alt"}, key, function()
+  hs.hotkey.bind(application_select, key, function()
     hs.application.launchOrFocus(app)
   end)
 end
