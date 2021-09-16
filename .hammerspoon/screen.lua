1b0ad800c080511905a56e3ebda958b6f5893af4
Ari Sweedler <ari@sweedler.com>
2021-09-15 20:22:26 -0700

    Home 9/15

--- .hammerspoon/screen.lua
+++ .hammerspoon/screen.lua
@@ -1,4 +1,32 @@
 -- Print all screens
-for key,value in pairs(hs.screen.allScreens()) do
-  print(value)
+-- for key,value in pairs(hs.screen.allScreens()) do
+--   print(value)
+-- end
+
+-- Create a new 'Screen space'
+function add_screen()
+hs.osascript.applescript([[
+do shell script "open -a 'Mission Control'"
+delay 0.5
+tell application "System Events" to
+    click (every button whose value of attribute "AXDescription" is "add desktop")
+      of UI element "Spaces Bar" of UI element 1 of group 1 of process "Dock"
+delay 0.5
+do shell script "open -a 'Mission Control'"
+]])
 end
+
+-- Remove the current 'Screen space'
+-- function remove_screen()
+-- hs.osascript.applescript([[
+-- do shell script "open -a 'Mission Control'"
+-- delay 0.5
+-- tell application "System Events" to
+--     click (every button whose value of attribute "AXDescription" is "add desktop")
+--       of UI element "Spaces Bar" of UI element 1 of group 1 of process "Dock"
+-- delay 0.5
+-- do shell script "open -a 'Mission Control'"
+-- ]])
+-- end
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "N", add_screen)
---
+++ .vim/after/ftplugin/netrw.vim
@@ -0,0 +1,5 @@
+" Set this to hide files in netrw that gitignore ignores. What the heck!?
+" so OP!
+" let g:netrw_list_hide = netrw_gitignore#Hide()
+
+setlocal relativenumber
