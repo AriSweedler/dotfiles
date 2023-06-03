1d2084b5c250157cfb1e65883f3da689d334568f
Ari Sweedler <ari@sweedler.com>
2023-06-03 01:50:25 -0700

    Added shell lib in 'power'. Added 'sonic' commands

--- .config/zsh-init/xdg.zsh
+++ .config/zsh-init/xdg.zsh
@@ -1,21 +1,13 @@
 # XDG best practices:
 # https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
-#
-# If $XDG_DATA_HOME is either not set or empty, a default equal to
-# $HOME/.local/share should be used
-export XDG_DATA_HOME="$HOME/.local"
-
-#If $XDG_CONFIG_HOME is either not set or empty, a default equal to
-# $HOME/.config should be used
+export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CONFIG_HOME="$HOME/.config"
 
 # XDG_DATA_DIRS
-# If $XDG_CONFIG_HOME is either not set or empty, a default equal to $HOME/.config should be used
+# If $XDG_DATA_DIRS is either not set or empty, a value equal to /usr/local/share/:/usr/share/ should be used.
 #
 # XDG_CONFIG_DIRS
 # If $XDG_CONFIG_DIRS is either not set or empty, a value equal to /etc/xdg should be used.
 #
 # XDG_CACHE_HOME
 # If $XDG_CACHE_HOME is either not set or empty, a default equal to $HOME/.cache should be used.
-#
-# XDG_CACHE_HOME
