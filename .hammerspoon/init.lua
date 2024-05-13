55db2ec18e9a566945615b1952034024769ff2c9
Ari Sweedler <ari.sweedler@illumio.com>
2024-05-13 10:49:44 -0700

    Removed some files

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,10 +1,10 @@
-require "easy-reload"             -- reload hammerspoon config
-require "volume-and-brightness"   -- change these
-require "application"             -- easy open apps
-require "gpt"                     -- prompt_engineering snippets
-require "email"                   -- paste my email easily
-require "logyank"                 -- yank from a LOG:: line | data=blah
-require "rocket_league"           -- spam emotes lol
-require "macros"                  -- macro library & some special ones
+require("easy-reload") -- reload hammerspoon config
+require("volume-and-brightness") -- change these
+require("application") -- easy open apps
+require("gpt") -- prompt_engineering snippets
+require("email") -- paste my email easily
+require("logyank") -- yank from a LOG:: line | data=blah
+require("rocket_league") -- spam emotes lol
+-- require "macros"                  -- macro library & some special ones
 --------------------------------- Experimenting --------------------------------
 -- require "noises"
