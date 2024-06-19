8bf705eabc4fec8c2eb9bb3d860d74943f4660fd
Ari Sweedler <ari.sweedler@illumio.com>
2024-06-19 00:44:17 -0700

    Work changes

---
+++ .config/nvim/lua/plugins/dbee.lua
@@ -0,0 +1,14 @@
+local M = {
+	"kndndrj/nvim-dbee",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+	},
+	build = function()
+		require("dbee").install()
+	end,
+	config = function()
+		require("dbee").setup()
+	end,
+}
+
+return M
