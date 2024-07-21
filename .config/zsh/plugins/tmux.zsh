b6393b21853b8ec7441f4b43b9815349387c714c
Ari Sweedler <ari@sweedler.com>
2024-07-21 16:03:49 -0700

    Bugfix XDG for TPM. Differentiate between TMUX_PLUGIN_MANAGER_PATH and *_ROOT
    * TMUX_PLUGIN_MANAGER_PATH is actually used by the repo
    * I just created the name TMUX_PLUGIN_MANAGER_ROOT for my own use
      * Can't believe they used XDG_CONFIG_HOME instead of XDG_DATA_HOME by
        default. That's so lame - what a mistake.

--- .config/zsh/plugins/tmux.zsh
+++ .config/zsh/plugins/tmux.zsh
@@ -1,16 +1,26 @@
-# Ensure tpm is installed
-export TPM_ROOT="$XDG_DATA_HOME/tmux/tpm"
+# Change the install location of tpm. Keep these in sync with
+# ~/.config/tmux/plugin/tpm.conf
+export TMUX_DATA_DIR="$XDG_DATA_HOME/tmux"
+export TMUX_PLUGIN_MANAGER_ROOT="$TMUX_DATA_DIR/tpm"
+export TMUX_PLUGIN_MANAGER_PATH="$TMUX_DATA_DIR/tpm_plugins"
+
 function ensure_tpm_installed() {
-  [ -d "$TPM_ROOT" ] && return
+  [ -d "$TMUX_PLUGIN_MANAGER_ROOT" ] && return
 
   function install_tpm() {
-    log::info "Installing the Tmux Plugin Manager (tpm) | TPM_ROOT='$TPM_ROOT'"
-    git clone https://github.com/tmux-plugins/tpm "$TPM_ROOT"
+    log::info "Installing the Tmux Plugin Manager (tpm) | TMUX_PLUGIN_MANAGER_ROOT='$TMUX_PLUGIN_MANAGER_ROOT'"
+    git clone https://github.com/tmux-plugins/tpm "$TMUX_PLUGIN_MANAGER_ROOT"
   }
   if ! install_tpm; then
     log::err "Failed to install tpm"
   fi
   unset -f install_tpm
+
+  # Install tpm plugins
+  if ! "${TMUX_PLUGIN_MANAGER_ROOT}/bindings/install_plugins"; then
+    log::err "Failed to install tpm plugins. Try 'Prefix' + 'Capital I'"
+    return 1
+  fi
 }
 ensure_tpm_installed
 unset -f ensure_tpm_installed
