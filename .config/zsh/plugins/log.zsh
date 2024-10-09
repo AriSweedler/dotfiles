cf89e3c91312ceda720528b0cff8b5854cbbb08c
Ari Sweedler <ari.sweedler@illumio.com>
2024-10-09 12:03:30 -0700

    Better logging

--- .config/zsh/plugins/log.zsh
+++ .config/zsh/plugins/log.zsh
@@ -1,42 +1,38 @@
 # The classic log suite
-c_red='\e[31m'
-c_green='\e[32m'
-c_yellow='\e[33m'
-c_cyan='\e[36m'
-c_rst='\e[0m'
+c_red='\e[31m'; c_green='\e[32m'; c_yellow='\e[33m'; c_cyan='\e[36m'; c_rst='\e[0m'
+log::preamble::bash() { echo -n "[$(date "+%Y-%m-%dT%T.000Z")] [${BASH_SOURCE[2]}]" ; }
+log::preamble()       { echo -n "[$(date "+%Y-%m-%dT%T.000Z")] [$funcstack[3]]" ; }
 
-bash_preamble() { echo -n "[$(date "+%Y-%m-%dT%T.000Z")] [${BASH_SOURCE[2]}]" ; }
-preamble() { echo -n "[$(date "+%Y-%m-%dT%T.000Z")] [$funcstack[3]]" ; }
-log::dev() { echo -e "${c_cyan}[DEV] $(preamble)${c_rst}" "$@" >&2 ; }
-log::err() { echo -e "${c_red}[ERROR] $(preamble)${c_rst}" "$@" >&2 ; }
-log::info() { echo -e "${c_green}[INFO] $(preamble)${c_rst}" "$@" >&2 ; }
-log::debug() { [ -z "$ARI_DEBUG" ] && return; echo -e "${c_grey}[DEBUG] $(preamble)${c_rst}" "$@" >&2 ; }
-log::warn() { echo -e "${c_yellow}[WARN] $(preamble)${c_rst}" "$@" >&2 ; }
+log::dev()      { echo -e "${c_cyan}[DEV] $(log::preamble)${c_rst}" "$@" >&2 ; }
+log::err()     { echo -e "${c_red}[ERROR] $(log::preamble)${c_rst}" "$@" >&2 ; }
+log::warn()  { echo -e "${c_yellow}[WARN] $(log::preamble)${c_rst}" "$@" >&2 ; }
+log::info()   { echo -e "${c_green}[INFO] $(log::preamble)${c_rst}" "$@" >&2 ; }
+log::_debug() { echo -e "${c_grey}[DEBUG] $(log::preamble)${c_rst}" "$@" >&2 ; }
+log::debug() { [ -z "${DEBUG:-}" ] && return; log::_debug "$@" ; }
 
-run_cmd() {
+# Multiline logs
+log::DEV()   { while read -r line; do log::dev   "$line"; done <<< "$*" ; }
+log::ERR()   { while read -r line; do log::err   "$line"; done <<< "$*" ; }
+log::WARN()  { while read -r line; do log::warn  "$line"; done <<< "$*" ; }
+log::INFO()  { while read -r line; do log::info  "$line"; done <<< "$*" ; }
+log::DEBUG() { while read -r line; do log::debug "$line"; done <<< "$*" ; }
+
+# Running commands
+function run_cmd() {
   log::${lvl:-info} "$@"
-  "$@" && return
-  rc=$?
-  log::${lvl_fail:-err} "cmd '$*' failed: $rc"
+  "$@" && return; rc=$?
+  set -- $(printf "'%s' " "$@")
+  log::${lvl_fail:-ERR} "cmd failed | rc=$rc cmd_quoted=|$*|"
   return $rc
 }
 
-function log::DEV() {
-  while read -r line; do log::dev "$line"; done <<< "$*"
+# String manipulation
+function _prepend() {
+  [ -t 0 ] && return 1
+  while read -r line; do echo "${1:?}${line}"; done
 }
-
-function log::ERR() {
-  while read -r line; do log::err "$line"; done <<< "$*"
-}
-
-function log::WARN() {
-  while read -r line; do log::warn "$line"; done <<< "$*"
-}
-
-function log::INFO() {
-  while read -r line; do log::info "$line"; done <<< "$*"
-}
-
-function log::DEBUG() {
-  while read -r line; do log::debug "$line"; done <<< "$*"
+function _indent() {
+  local indent=""
+  for ((i="${1:?}"; i-->0;)); indent+=" "
+  _prepend "${indent}"
 }
