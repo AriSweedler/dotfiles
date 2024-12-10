522c6e6ec0fe7ecfc3515fef36d03533401d2978
Ari Sweedler <ari@sweedler.com>
2024-12-10 13:22:17 -0800

    Invoke 'cc' with 'CTRL-O'

--- .config/zsh/plugins/double_cd.zsh
+++ .config/zsh/plugins/double_cd.zsh
@@ -62,6 +62,11 @@ function double_cd::generate() {
 
       # cX function
       cat << EOF > "${dcd_functions}/c$y"
+if (( \$# == 0 )); then
+  choice="\$(ls  '$y_expand' | fzf)"
+  cd "${y_expand}/\${choice}"
+  return
+fi
 double_cd '$y_expand' "\$@"
 EOF
 
