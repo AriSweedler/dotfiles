984e5aef0f845683b4acba54df029e190969fc8c
Ari Sweedler <ari@sweedler.com>
2025-03-03 22:16:46 +0900

    Set formatops for go

--- .config/nvim/after/ftplugin/go.lua
+++ .config/nvim/after/ftplugin/go.lua
@@ -6,3 +6,13 @@ require("ari.lsp.gopls")
 
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+-- Put in the comment leader when hitting '<Enter>' while in insert mode on a comment
+vim.opt.formatoptions:append("r")
+-- Put in the comment leader when hitting 'O' while in normal mode on a comment
+vim.opt.formatoptions:append("o")
+-- a	Automatic formatting of paragraphs.  Every time text is inserted or
+-- 	deleted the paragraph will be reformatted.  See |auto-format|.
+-- 	When the 'c' flag is present this only happens for recognized
+-- 	comments.
+-- vim.opt.formatoptions:append("a")
