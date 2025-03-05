0b2216416cacf221e03adde9f94a2818c80fa51c
Ari Sweedler <ari@sweedler.com>
2025-03-05 21:30:46 +0900

    Wrote fzfdb_cli. Use it for 'pubkey'
    * Also using it at Illumio. But that will not get committed, as it is
      specific to work. I use it for 'ilo_env', which lets me easily set
      environment variables for various actions (like downloading from an
      s3) without having to ever show the env vars (useful for giving
      recorded demos)
    * Previously, these scripts were copy/pasted and forked. But now, I have
      an engine and a few usages of them. Working like a charm.
    * Plans to move double_cd over to this system

--- .config/zsh/plugins/pubkey.zsh
+++ .config/zsh/plugins/pubkey.zsh
@@ -1,114 +1,37 @@
-function _pk::help() {
-  cat <<EOF
-Usage: pubkey [OPTIONS] [PUBKEY]
+function pubkey() {
+  fzfdb --fzfdb_dir "${XDG_DATA_HOME}/ssh" -- "$@"
+}
 
-Options:
-  -h, --help  Show this help message and exit
-  --edit      Edit pubkey file
-  --copy      Copy pubkey to clipboard
-  --dir       Show the directory where pubkeys are stored
-  --list      List all pubkeys in your database
-  --compare   Use 'rg' to check if a pattern shows up in your pubkey database
+function pubkey::action::help() {
+  cat << EOF
+USAGE: pubkey [pubkey]
+ 
+Put a pubkey on your clipboard. Use fzf to search through all possible
+pubkeys that have been saved
+ 
+$(fzfdb::help_common)
 EOF
 }
 
-function pubkey() {
-  local action=copy
-  local rpp=1 mpp=1 pp=() # [required/max] positional parameters
+# Input: pubkey
+# Output: the line you want to show up in fzf
+function pubkey::key::menuitem() {
+  local pubkey="${1:?}"
+  local pubkey_file="$(fzfdb::key::_path "${pubkey}")"
 
-  while (( $# > 0 )); do
-    case "$1" in
-      -h|--help) _pk::help; return 0;;
-      --edit) action=edit; rpp=0; mpp=1; shift;;
-      --copy) action=copy; rpp=1; mpp=1; shift;;
-      --list) action=list; rpp=0; mpp=0; shift;;
-      --dir) action=dir; rpp=0; mpp=0; shift;;
-      --compare) action=compare; rpp=1; mpp=1; shift;;
-      *)
-        pp+=( "$1" ) && shift
-        if (( ${#pp[@]} > mpp )); then
-          log::err "Maximum of N positional parameters | N='${mpp}'"
-          return 1
-        fi
-        ;;
-    esac
-  done
-
-  if (( ${#pp[@]} < rpp )); then
-    log::err "Required N positional parameters | N='${rpp}' #pp='${#pp[@]}'"
-    return 1
-  fi
-
-  "_pk::action::$action" "${pp[@]}"
+  local pubkey_body
+  pubkey_body=$(awk '{print $1 " " $3}' "${pubkey_file}" | tr -s '\n' ';')
+  echo "${pubkey} | ${pubkey_body}"
 }
 
-function validate::pubkey() {
-  local pk="${1:?}"
-  local pk_abs
-  pk_abs="$(_pk::dir)/$pk"
-  [ -r "$pk_abs" ]
-}
+# Input: pubkey
+# Put the pubkey on the system clipboard & log
+function pubkey::key::_handle_selection() {
+  local pubkey="${1:?}"
+  local pubkey_file="$(fzfdb::key::_path "${pubkey}")"
 
-function _pk::dir() {
-  echo "${XDG_DATA_HOME:-$HOME/.local/share}/ssh/pubkeys_of_friends"
-}
-
-function _pk::file() {
-  local pk="${1:?}"
-  validate::pubkey "$pk" || fatal::_pk::invalid
-
-  echo "$(_pk::dir)/$pk"
-}
-
-function _pk::action::copy() (
-  local pk="${1:?}"
-  validate::pubkey "$pk" || fatal::_pk::invalid
-  local pk_abs
-  pk_abs="$(_pk::file "$pk")"
-
-  pbcopy < "$pk_abs"
+  pbcopy < "${pubkey_file}"
   log::INFO "Copied friend's pubkey to clipboard
-friend='${c_cyan}${pk}${c_rst}'
+friend='${c_cyan}${pubkey}${c_rst}'
 pubkey='${c_green}$(pbpaste)${c_rst}'"
-)
-
-function _pk::action::edit() (
-  if (( $# == 0 )); then
-    "$EDITOR" "$(_pk::dir)"
-    return
-  fi
-
-  local pk="${1:-}"
-  local pk_abs
-  pk_abs="$(_pk::dir)/$pk"
-
-  run_cmd "$EDITOR" "$pk_abs"
-  pubkey --copy "$pk"
-)
-
-function _pk::action::list() {
-  ls -1 "$(_pk::dir)"
-}
-
-function _pk::action::dir() {
-  _pk::dir
-}
-
-function _pk::action::compare() {
-  local pk_contents="${1:?}"
-  local files_with_matches
-  files_with_matches=($(
-    cd "$(_pk::dir)"
-    rg "${pk_contents}" --files-with-matches
-  ))
-  log::info "Found N matches | N='${#files_with_matches[@]}'"
-  case "${#files_with_matches[@]}" in
-    0) return 1;;
-    *) log::INFO "${(F)files_with_matches}" ;;
-  esac
-}
-
-function fatal::_pk::invalid() {
-  log::err "Invalid pubkey | pk='${pk}'"
-  exit 1
 }
