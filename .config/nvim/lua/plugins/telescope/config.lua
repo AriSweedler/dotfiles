f5382d3ca9c4e0082831e0e2df21b14ecdef2a3c
Ari Sweedler <ari@sweedler.com>
2025-05-11 22:55:58 -0700

    Transition from telescope to snacks

--- .config/nvim/lua/plugins/telescope/config.lua
+++
@@ -1,8 +0,0 @@
--- Enable telescope fzf native, if installed
-pcall(require("telescope").load_extension, "fzf")
-pcall(require("telescope").load_extension, "ui-select")
-
-require("plugins.telescope.files")
-require("plugins.telescope.search")
-require("plugins.telescope.fun")
-require("plugins.telescope.lsp")
--- .config/nvim/lua/plugins/telescope/lsp.lua
+++
@@ -1,6 +0,0 @@
-local builtin = require("telescope.builtin")
-
-vim.keymap.set("n", "<Leader>SLr", builtin.lsp_references, { desc = "[S]earch [L]SP [r]eferences" })
-vim.keymap.set("n", "<Leader>SLi", builtin.lsp_incoming_calls, { desc = "[S]earch [L]SP [i]ncoming calls" })
-vim.keymap.set("n", "<Leader>SLo", builtin.lsp_outgoing_calls, { desc = "[S]earch [L]SP [o]utgoing calls" })
-vim.keymap.set("n", "<Leader>SLI", builtin.lsp_implementations, { desc = "[S]earch [L]SP [I]mplementations" })
