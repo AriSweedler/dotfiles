b163b24999c6510e99b8649b94106d578edf090e
Ari Sweedler <ari@sweedler.com>
2025-06-15 19:21:12 -0700

    Clean up karabiner a bit

--- .config/karabiner/karabiner.ts/src/index.ts
+++ .config/karabiner/karabiner.ts/src/index.ts
@@ -1,19 +1,10 @@
 import { map, rule, writeToProfile } from "karabiner.ts"
-import { hyper_c, raycast_deeplink } from "./utils/macros"
 import applicationMode from "./modes/application"
 import karabinerMode from "./modes/karabiner"
 import windowMode from "./modes/window"
 
 writeToProfile("Default", [
-  rule('Right option → Hyper').manipulators([
-    map('right_option').toHyper().toIfAlone('right_option'),
-  ]),
-  rule('confetti').manipulators([{
-    type: "basic",
-    from: hyper_c,
-    to: [raycast_deeplink("extensions/raycast/raycast/confetti")],
-  }]),
-
+  rule('Right option → Hyper').manipulators(map('right_option').toHyper()),
   applicationMode,
   karabinerMode,
   windowMode,
