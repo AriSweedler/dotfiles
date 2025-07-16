722d5f8678b9fb03c40ae6e16f20c93ac8698b08
Ari Sweedler <ari@sweedler.com>
2025-07-16 14:25:37 -0700

    Karabiner improvements

--- .config/karabiner/karabiner.ts/src/utils/macros.ts
+++ .config/karabiner/karabiner.ts/src/utils/macros.ts
@@ -1,25 +1,28 @@
 import { fileURLToPath } from "url"
 import fs from "fs"
 import path from "path"
+import os from "os"
 
 const karabinerRoot = path.resolve(fileURLToPath(import.meta.url), "../../..")
+
 console.log("Karabiner root path:", karabinerRoot)
 
 // Local script runner
-export const karabiner_script = (scriptPath: string) => {
-  const fullPath = path.resolve(karabinerRoot, `src/scripts/bin/${scriptPath}`)
-  // make sure this file is executable - check permission bits
+export const karabiner_script = (scriptPathRel: string) => {
+  const scriptPathAbs = path.resolve(karabinerRoot, `src/scripts/bin/${scriptPathRel}`)
+  const scriptPathAbsEnv = scriptPathAbs.replace(os.homedir(), "$HOME")
 
-  // Check if file is executable
+  // make sure this file is executable - check permission bits
   try {
-    fs.accessSync(fullPath, fs.constants.X_OK)
+    const scriptPathAbs = path.resolve(karabinerRoot, `src/scripts/bin/${scriptPathRel}`)
+    fs.accessSync(scriptPathAbs, fs.constants.X_OK)
   } catch (err) {
-    throw new Error(`Script is not executable or not found | scriptPath=${scriptPath}, fullPath=${fullPath}`)
+    throw new Error(`Script is not executable or not found | scriptPathRel=${scriptPathRel}, scriptPathAbs=${scriptPathAbs} scriptPathAbsEnv=${scriptPathAbsEnv}`)
   }
 
-  const logFile = `/tmp/karabiner.${scriptPath}.txt`
+  const logFile = `/tmp/karabiner.${scriptPathRel}.txt`
 
   return {
-    shell_command: `export LOG_FILE=${logFile}; export REPO_ROOT=${karabinerRoot}; ${fullPath}`
+    shell_command: `export LOG_FILE=${logFile}; export REPO_ROOT=${karabinerRoot.replace(os.homedir(), "$HOME")}; ${scriptPathAbsEnv}`
   }
 }
