6b366428e92b1949d83d5e69b228ede9dc277306
Ari Sweedler <ari.sweedler@airtable.com>
2025-10-10 03:12:02 -0700

    Added some devices

--- .config/karabiner/karabiner.ts/src/homerow.ts
+++ .config/karabiner/karabiner.ts/src/homerow.ts
@@ -1,14 +1,27 @@
-import { map, rule } from "karabiner.ts"
+import { DeviceIdentifier, FromAndToKeyCode, Manipulator, map, rule } from "karabiner.ts"
 
-// Device-specific condition
-const kinesisDevice = {
-  is_keyboard: true,
-  product_id: 37904,
-  vendor_id: 1423,
+// Device identifiers
+//
+// To find a new one, go to karabiner-EventViewer > Devices & find your device.
+// It may be helpful to use jq to search through the JSON blob
+//
+//     jq '.[] | select(.manufacturer == "Kinesis")'
+//
+const devices: Record<string, DeviceIdentifier> = {
+  kinesisFreestylePro: {
+    is_keyboard: true,
+    product_id: 258,
+    vendor_id: 10730,
+  },
+  kinesisFreestyle2: {
+    is_keyboard: true,
+    product_id: 37904,
+    vendor_id: 1423,
+  }
 }
 
 // Helper to swap two keys, preserving any modifiers
-const swapKeys = (keyA: string, keyB: string): Manipulator[] => [
+const swapKeys = (keyA: FromAndToKeyCode, keyB: FromAndToKeyCode): Manipulator[] => [
   {
     type: 'basic',
     from: {
@@ -27,14 +40,14 @@ const swapKeys = (keyA: string, keyB: string): Manipulator[] => [
   },
 ];
 
-export default homeRow = [
+export const homeRow = [
   rule('Right option → Hyper').manipulators([
     map('right_option').toHyper().toIfAlone('right_option'),
   ]),
   rule('Kinesis swaps command and option')
     .condition({
       type: 'device_if',
-      identifiers: [kinesisDevice],
+      identifiers: Object.values(devices),
     })
     .manipulators([
       ...swapKeys('left_command', 'left_option')
