cad1919b83ffc32a7fed4a749a424ae72319f4c7
Ari Sweedler <ari.sweedler@airtable.com>
2025-10-23 13:14:12 -0700

    Add tmux session management to karabiner-hello-world script
    Implement ensure_session function with two subfunctions:
    - ensure_session::created - creates session if it doesn't exist (early exit pattern)
    - ensure_session::_switch - switches to the session

--- .config/nvim/lua/plugins/gitsigns/config.lua
+++ .config/nvim/lua/plugins/gitsigns/config.lua
@@ -31,14 +31,47 @@ function M.on_attach_hook(bufnr)
 	my_map({ "n", "v" }, "]h", brack_h("]h", gs.next_hunk), { expr = true, desc = "Jump to next hunk" })
 	my_map({ "n", "v" }, "[h", brack_h("[h", gs.prev_hunk), { expr = true, desc = "Jump to previous hunk" })
 
+	-- NOTE: ]c is much more ergonomic for me than ]h, so I make this one
+	-- exception to the grammar...
+	my_map({ "n", "v" }, "]c", brack_h("]c", gs.next_hunk), { expr = true, desc = "Jump to next [c]hunk" })
+	my_map({ "n", "v" }, "[c", brack_h("[c", gs.prev_hunk), { expr = true, desc = "Jump to previous [c]hunk" })
+
+	-- Unified hunk staging function
+	local function stage_hunk_with_feedback()
+		local mode = vim.fn.mode()
+		local range
+
+		-- If in visual mode, determine selected line range
+		if mode == 'v' or mode == 'V' then
+			local start_line = vim.fn.line('.')
+			local end_line = vim.fn.line('v')
+			if start_line > end_line then
+				start_line, end_line = end_line, start_line
+			end
+			range = { start_line, end_line }
+		end
+
+		-- Stage hunk (with or without range)
+		if range then
+			gs.stage_hunk(range)
+		else
+			gs.stage_hunk()
+		end
+
+		-- Notify and schedule redraw
+		-- vim.notify("We will schedule a redraw", vim.log.levels.INFO)
+		vim.schedule(function()
+			-- vim.notify("Redrawing now", vim.log.levels.INFO)
+			vim.api.nvim_command('redraw')
+		end)
+	end
+
 	-- Hunk actions
-	my_map("v", "<Leader>hs", function()
-		gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-	end, { desc = "stage git hunk" })
+	my_map("v", "<Leader>hs", stage_hunk_with_feedback, { desc = "stage git hunk" })
 	my_map("v", "<Leader>hr", function()
 		gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
 	end, { desc = "reset git hunk" })
-	my_map("n", "<Leader>hs", gs.stage_hunk, { desc = "git stage hunk" })
+	my_map("n", "<Leader>hs", stage_hunk_with_feedback, { desc = "stage git hunk" })
 	my_map("n", "<Leader>hS", gs.undo_stage_hunk, { desc = "undo stage hunk" })
 	my_map("n", "<Leader>hr", gs.reset_hunk, { desc = "git reset hunk" })
 	my_map("n", "<Leader>hp", gs.preview_hunk, { desc = "preview git hunk" })
