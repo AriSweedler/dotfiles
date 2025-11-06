84ddc6b928748c7295e6044b305baf8f858f9ead
Ari Sweedler <ari.sweedler@airtable.com>
2025-11-05 17:48:03 -0800

    updated jq playground

--- .config/nvim/lua/plugins/jq-playground.lua
+++ .config/nvim/lua/plugins/jq-playground.lua
@@ -8,7 +8,7 @@ local M = {
 
 -- Escape single quotes for a single-quoted shell string:
 -- ' -> '"'"'
-local function jq_escape(str)
+local function xq_escape(str)
 	return str:gsub("'", "'\"'\"'")
 end
 
@@ -17,44 +17,71 @@ local bufmap = function(lhs, rhs, desc, opts)
 	opts = opts or {}
 	opts.buffer = true
 	opts.desc = "JqPlayground: " .. desc
-	vim.keymap.set("n", lhs, rhs, opts)
+	local mode = opts.mode or 'n'
+	opts.mode = nil
+	vim.keymap.set(mode, lhs, rhs, opts)
 end
 
-local function ftplugin_jq()
-	-- TODO: Know what file we are attached to...
-
+local function ftplugin_xq()
 	bufmap("Y", function()
 		-- Grab full buffer
 		local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 		local content = table.concat(lines, "\n")
-		local escaped = jq_escape(content)
+		local escaped = xq_escape(content)
 
 		-- Build command and copy to clipboard
-		local cmd = "jq '" .. escaped .. "'"
+		local cmd = vim.o.filetype .. " '" .. escaped .. "' " .. vim.api.nvim_buf_get_name(vim.b.parent_bufnr)
 		vim.fn.setreg("+", cmd)
 
-		vim.notify("Copied jq command: " .. cmd, vim.log.levels.INFO)
-	end, "Copy buffer into jq command")
+		vim.notify("Copied " .. vim.o.filetype .. " command: " .. cmd, vim.log.levels.INFO)
+	end, "Copy buffer into " .. vim.o.filetype .. " command")
 
 	-- We don't want to save this buffer
-	bufmap("<C-f>", "<Plug>(JqPlaygroundRunQuery)", "run query")
+	bufmap("<C-f>", "<Plug>(JqPlaygroundRunQuery)", "run query", { mode = { "i", "n" } })
 	vim.bo.buftype = "nofile"
 	vim.bo.swapfile = false
 	vim.bo.bufhidden = "hide"
 end
 
-local function ftplugin_json()
-	bufmap("Q", vim.cmd.JqPlayground, "Open")
+local function get_xq_query()
+	local filetype = vim.bo.filetype
+
+	if filetype == "json" or filetype == "jsonl" then
+		return require("jsonpath").get()
+	elseif filetype == "yaml" or filetype == "yml" then
+		return require("yaml_nvim").get_yaml_key()
+	else
+		vim.notify("Unsupported filetype: " .. filetype, vim.log.levels.ERROR)
+		return nil
+	end
+end
+
+local function jqPlaygroundOpenSeeded()
+	-- read the jq/yq path that we are on
+	local xq_query = get_xq_query()
+	xq_query = "." .. (xq_query or '')
+
+	vim.cmd.JqPlayground()
+
+	-- Insert the yq/jq path as the seed of our query
+	vim.schedule(function()
+		vim.api.nvim_put({ xq_query }, "c", false, true)
+	end)
+end
+
+local function ftplugin_jqplayground_init()
+	bufmap("Q", jqPlaygroundOpenSeeded, "Open with file path")
+	bufmap("<Leader>Q", vim.cmd.JqPlayground, "Open")
 end
 
 function M.init()
 	vim.api.nvim_create_autocmd("FileType", {
-		pattern = "jq",
-		callback = ftplugin_jq,
+		pattern = { "jq", "yq" },
+		callback = ftplugin_xq,
 	})
 	vim.api.nvim_create_autocmd("FileType", {
-		pattern = "json",
-		callback = ftplugin_json,
+		pattern = { "json", "jsonl", "yaml" },
+		callback = ftplugin_jqplayground_init,
 	})
 end
 
--- .config/zsh/plugins/zsh-vi-mode.zsh
+++ .config/zsh/plugins/zsh-vi-mode.zsh
@@ -1,16 +1,26 @@
-source $(brew --prefix)/opt/zsh-vi-mode/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
 function zvm_config() {
-  ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT
+  ZVM_LINE_INIT_MODE="$ZVM_MODE_INSERT"
+  ZVM_VI_INSERT_ESCAPE_BINDKEY="jk"
+  ZVM_SYSTEM_CLIPBOARD_ENABLED="true"
+  ZVM_CLIPBOARD_COPY_CMD=pbcopy
+  ZVM_CLIPBOARD_PASTE_CMD=pbpaste
+  ZVM_INSERT_MODE_CURSOR=$ZVM_CURSOR_BEAM
+  ZVM_NORMAL_MODE_CURSOR=$ZVM_CURSOR_BLOCK
+}
 
-  ZVM_VI_INSERT_ESCAPE_BINDKEY=jk
+function zvm_after_init() {
+  fzf::init
 
-  zle -N delete-entire-line
+  # TODO: Can we configure changing the cursor when we enter 'menu select' mode?
+
   function delete-entire-line {
     zle beginning-of-line
     zle kill-line
   }
+  zle -N delete-entire-line
   bindkey -M vicmd 'D' delete-entire-line
-}
-zvm_config
 
-zvm_after_init_commands+=('fzf::init')
+  # TODO: Can we configure changing the cursor when we enter 'menu select' mode?
+}
+
+source $(brew --prefix)/opt/zsh-vi-mode/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
