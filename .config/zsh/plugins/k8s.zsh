4183c7fbdc11aab713953e0c54822bf953398086
Ari Sweedler <ari.sweedler@airtable.com>
2025-11-13 14:05:38 -0800

    git and k8s

--- .config/zsh/plugins/k8s.zsh
+++ .config/zsh/plugins/k8s.zsh
@@ -1,1 +1,22 @@
 eval "$(kubectl completion zsh)"
+
+# brew install kubectx
+alias kns="kubens"
+alias kctx="kubectx"
+alias kx="kubectx"
+alias k="kubectl"
+
+function kxx() {
+  # get current context and namespace
+  local ctx ns
+  ctx="$(kubectl config current-context)"
+  ns="$(kubectl config view --minify --output 'jsonpath={..namespace}')"
+
+  # if namespace is empty, default to 'default'
+  if [[ -z "$ns" ]]; then
+    ns="default"
+  fi
+
+  echo -n "kubectl --context $ctx --namespace $ns" | tee >(cat) | pbcopy
+  echo
+}
