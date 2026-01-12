96550487c77b820b05035ff1e35950ef9a1d652a
Ari Sweedler <ari.sweedler@airtable.com>
2026-01-12 12:00:54 -0800

    tmux command management shell functions

--- .config/zsh/plugins/tmux.zsh
+++ .config/zsh/plugins/tmux.zsh
@@ -29,3 +29,40 @@ function tmux::rename-window() {
   [ -z "$TMUX" ] && return
   tmux rename-window "$@"
 }
+
+function tmux::clear() {
+  clear
+  tmux clear-history
+  log::info "Cleared scrollback buffer"
+}
+
+function tmux::cap() {
+  local file="${TMUX_CAPTURE_FILE:-"$HOME/Desktop/capture.txt"}"
+
+  log::info "Capturing scrollback buffer to file | file_var='TMUX_CAPTURE_FILE' file='${c_grey}${file}${c_rst}'"
+  tmux capture-pane -p -S - > "${file}"
+}
+
+function tmux::cap_one() {
+  [ -z "$TMUX" ] && return
+
+  # Match your prompt line via an awk regex (❯)
+  local prompt_re="${TMUX_PROMPT_RE:-❮|❯}"
+
+  # Pipeline: Reverse text, capture from (first regex match (inclusive) to next
+  # (exclusive), un-reverse text, place on clipboard
+  #
+  # Result: Copy text from second-to-last prompt (inclusive) to last prompt
+  # (exclusive)
+  #
+  # NOTE: My PS1 is 2 lines, so I use '1,2' as the range in the 'sed d' command.
+  tmux capture-pane -p -S - \
+    | tac \
+    | awk -v re="$prompt_re" '
+      (!s && $0 ~ re && (s=NR)), (s && $0 ~ re && NR!=s)
+    ' \
+    | sed '1,2d' \
+    | tac \
+    | pbcopy
+  log::info "Copied previous command + output to clipboard | prompt_re_var='TMUX_PROMPT_RE' prompt_re='${prompt_re}'"
+}
